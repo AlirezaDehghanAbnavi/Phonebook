117f948e19ae264517fece7810edd8aa1317ff06
Alireza Dehghanabnavi <alirezadehghan93@yahoo.com>
2026-05-14 13:25:18 +0200

    Eslint modified

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,12 @@
-import globals from "globals";
-import pluginReact from "eslint-plugin-react";
-import { defineConfig } from "eslint/config";
+import globals from 'globals'
 
-export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,jsx}"], languageOptions: { globals: globals.browser } },
-  pluginReact.configs.flat.recommended,
-]);
+export default [
+  {
+    files: ['**/*.js'],
+    languageOptions: {
+      sourceType: 'commonjs',
+      globals: { ...globals.node },
+      ecmaVersion: 'latest',
+    },
+  },
+]
